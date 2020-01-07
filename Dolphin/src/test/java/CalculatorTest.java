d129e4da071f00e2e26e154f1c645986b177eeae
LordHjorth <Hjorthen96@hotmail.com>
2020-01-07 12:49:05 +0100

    ostehaps

---
+++ Dolphin/src/test/java/CalculatorTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class CalculatorTest {
+
+    @Test
+    void justAnExample() {
+
+        assertEquals(10, 10, "10 = 10");
+
+    }
+
+    @Test
+    void justAnExample2() {
+
+        assertEquals(20, 20, "20 = 20");
+
+    }
+}
